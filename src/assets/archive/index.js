be352476b2c72cb5547cb96c86a4b2a0c71498eb
theharsh999 <harshpatelji999@gmail.com>
2026-02-19 17:37:16 +0530

    Fixed Archive images and removed facebook from teams

--- src/assets/archive/index.js
+++ src/assets/archive/index.js
@@ -23,47 +23,47 @@ const ArchiveImages = {
 
   "2024-25": {
     // Councils
-    Nimish_Bhosale: require("./2024-25/Nimish_Bhosale.png"),
-    Raj_Yadav: require("./2024-25/Raj_Yadav.png"),
-    Rishika_Singh: require("./2024-25/Rishika_Singh.png"),
-    Kashish_Tiwari: require("./2024-25/Kashish_Tiwari.png"),
-    Kaustubh_Suryavanshi: require("./2024-25/Kaustubh_Suryavanshi.png"),
-    Ayush_Tripathi: require("./2024-25/Ayush_Tripathi.png"),
-    Rishit_Patel: require("./2024-25/Rishit_Patel.png"),
-    Ayush_Gupta: require("./2024-25/Ayush_Gupta.png"),
+    Nimish_Bhosale: require("./2024-25/Nimish_Bhosale.webp"),
+    Raj_Yadav: require("./2024-25/Raj_Yadav.webp"),
+    Rishika_Singh: require("./2024-25/Rishika_Singh.webp"),
+    Kashish_Tiwari: require("./2024-25/Kashish_Tiwari.webp"),
+    Kaustubh_Suryavanshi: require("./2024-25/Kaustubh_Suryavanshi.webp"),
+    Ayush_Tripathi: require("./2024-25/Ayush_Tripathi.webp"),
+    Rishit_Patel: require("./2024-25/Rishit_Patel.webp"),
+    Ayush_Gupta: require("./2024-25/Ayush_Gupta.webp"),
 
     // JC's
-    Krishna_Kulkarni: require("./2024-25/Krishna_Kulkarni.png"),
-    Yashashri_Mahendrakar: require("./2024-25/Yashashri_Mahendrakar.png"),
-    Rajkumar_Yadav: require("./2024-25/Rajkumar_Yadav.png"),
-    Shweta_Yadav: require("./2024-25/Shweta_Yadav.png"),
+    Krishna_Kulkarni: require("./2024-25/Krishna_Kulkarni.webp"),
+    Yashashri_Mahendrakar: require("./2024-25/Yashashri_Mahendrakar.webp"),
+    Rajkumar_Yadav: require("./2024-25/Rajkumar_Yadav.webp"),
+    Shweta_Yadav: require("./2024-25/Shweta_Yadav.webp"),
 
     // Technical and Creative Teams
-    Technical_Team_2024_25: require("./2024-25/Technical_Team.jpeg"),
-    Creative_Team_2024_25: require("./2024-25/Creative_Team.jpeg"),
+    Technical_Team_2024_25: require("./2024-25/Technical_Team.webp"),
+    Creative_Team_2024_25: require("./2024-25/Creative_Team.webp"),
   },
 
   "2025-26": {
     // Councils
-    Pragati_Jha: require("./2025-26/Pragati_Jha.png"),
-    Shweta_Yadav: require("./2025-26/Shweta_Yadav.png"),
-    Rajkumar_Yadav: require("./2025-26/Rajkumar_Yadav.png"),
-    Adarsh_Singh: require("./2025-26/Adarsh_Singh.png"),
-    Krishna_Kulkarni: require("./2025-26/Krishna_Kulkarni.png"),
-    Omwardhan_Jha: require("./2025-26/Omwardhan_Jha.png"),
-    Kshitij_Dubey: require("./2025-26/Kshitij_Dubey.png"),
-    Mangilal_Choudhary: require("./2025-26/Mangilal_Chaudhary.png"),
-    Prashant_Swamy: require("./2025-26/Prashant_Swamy.png"),
+    Pragati_Jha: require("./2025-26/Pragati_Jha.webp"),
+    Shweta_Yadav: require("./2025-26/Shweta_Yadav.webp"),
+    Rajkumar_Yadav: require("./2025-26/Rajkumar_Yadav.webp"),
+    Adarsh_Singh: require("./2025-26/Adarsh_Singh.webp"),
+    Krishna_Kulkarni: require("./2025-26/Krishna_Kulkarni.webp"),
+    Omwardhan_Jha: require("./2025-26/Omwardhan_Jha.webp"),
+    Kshitij_Dubey: require("./2025-26/Kshitij_Dubey.webp"),
+    Mangilal_Choudhary: require("./2025-26/Mangilal_Chaudhary.webp"),
+    Prashant_Swamy: require("./2025-26/Prashant_Swamy.webp"),
 
     // JC's
-    Ashutosh_Pandey: require("./2025-26/Ashu.png"),
-    Sejal_Gupta: require("./2025-26/Sejal.png"),
-    Gandharvi_Kalyankar: require("./2025-26/Gandharvi.png"),
-    Dev_Dubey: require("./2025-26/Dev.png"),
+    Ashutosh_Pandey: require("./2025-26/Ashu.webp"),
+    Sejal_Gupta: require("./2025-26/Sejal.webp"),
+    Gandharvi_Kalyankar: require("./2025-26/Gandharvi.webp"),
+    Dev_Dubey: require("./2025-26/Dev.webp"),
 
     // Technical and Creative Teams
-    Technical_Team_2025_26: require("./2025-26/Technical_Team.png"),
-    Creative_Team_2025_26: require("./2025-26/Creative_Team.png"),
+    Technical_Team_2025_26: require("./2025-26/Technical_Team.webp"),
+    Creative_Team_2025_26: require("./2025-26/Creative_Team.webp"),
   },
 };
 export default ArchiveImages;
